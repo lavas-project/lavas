55daf4c4bd9f91c887ec2e6a1f34234b2046bbc5
xiaoiver <pyqiverson@gmail.com>
2018-01-10 10:04:31 +0800

    feat: expose render function

--- packages/lavas-core-vue/core/middlewares/ssr.js
+++ packages/lavas-core-vue/core/middlewares/ssr.js
@@ -21,25 +21,20 @@ export default function (core) {
         }
 
         let url = req.url;
+        let errorHandler = err => next(err);
 
         console.log(`[Lavas] route middleware: ssr ${url}`);
-        let matchedRenderer = await renderer.getRenderer();
-        let errorHandler = err => next(err);
-        let ctx = {
-            title: 'Lavas', // default title
+
+        let {err, html} = await renderer.render({
             url,
-            config: builder && builder.config || config, // mount config to ctx which will be used when rendering template
             req,
             res,
             error: errorHandler
-        };
-
-        // render to string
-        matchedRenderer.renderToString(ctx, (err, html) => {
-            if (err) {
-                return next(err);
-            }
-            res.end(html);
         });
+
+        if (err) {
+            return next(err);
+        }
+        res.end(html);
     };
 }
