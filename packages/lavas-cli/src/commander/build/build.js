c0e73027134d6d92e6835d318f8d743b75da9c27
Wangyisheng <wangyisheng@baidu.com>
2018-01-11 16:20:36 +0800

    fix: use fork instead of require to execute server.dev/prod.js

--- packages/lavas-cli/src/commander/build/build.js
+++ packages/lavas-cli/src/commander/build/build.js
@@ -14,9 +14,11 @@ const locals = require('../../locals')();
 /**
  * 处理 build 构建逻辑
  *
- * @param {Object} options action 的参数
+ * @param {string} config 用户指定的配置文件路径
  */
-module.exports = async function (options) {
+module.exports = async function (config) {
+    log.info(locals.START_BUILD + '...');
+
     let rootDir = utils.getLavasProjectRoot();
 
     // 通过 package.json 中的配置将 lavas-core 从命令行解耦
@@ -26,24 +28,17 @@ module.exports = async function (options) {
     let LavasCore = require(lavasConf.core || 'lavas-core-vue');
     let core = new LavasCore(rootDir);
 
-    log.info(locals.START_BUILD + '...');
-
-    options = options || {};
-    let initOptions = {};
-    if (options.config) {
-        let config = options.config;
+    if (config) {
         if (!path.isAbsolute(config)) {
-            config = path.resolve(utils.getLavasProjectRoot(), config);
+            config = path.resolve(process.cwd(), config);
         }
 
         if (!(await fs.pathExists(config))) {
             log.warn(`${locals.START_NO_FILE} ${config}`);
             return;
         }
-
-        initOptions.config = config;
     }
 
-    await core.init(process.env.NODE_ENV || 'production', true, initOptions);
+    await core.init(process.env.NODE_ENV || 'production', true, {config});
     await core.build();
 };
--- packages/lavas-cli/src/commander/server/index.js
+++ packages/lavas-cli/src/commander/server/index.js
@@ -7,11 +7,13 @@
 
 const path = require('path');
 const fs = require('fs-extra');
+const {fork} = require('child_process');
 
 const log = require('../../lib/utils/log');
 const utils = require('../../lib/utils');
 const locals = require('../../locals')();
 
+const DEFAULT_LAVAS_CONFIG = 'lavas.config.js';
 const DEFAULT_DEV_SERVER_SCRIPT = 'server.dev.js';
 const DEFAULT_PROD_SERVER_SCRIPT = 'server.prod.js';
 
@@ -19,18 +21,39 @@ module.exports = function (program) {
 
     // lavas dev
     program
-        .command('dev')
+        .command('dev [config]')
         .description(locals.START_DEV)
         .option('-p, --port <port>', locals.START_PORT)
-        .option('-c, --config [value]', locals.START_CONFIG)
-        .option('-s, --server-script <server-script>', locals.START_SCRIPT)
-        .action(async ({port, config, serverScript}) => {
-            let serverScriptPath = path.resolve(
-                utils.getLavasProjectRoot(),
-                serverScript || DEFAULT_DEV_SERVER_SCRIPT
-            );
-
+        .action(async (config, {port}) => {
             log.info(locals.START_DEV_SERVER + '...');
+
+            // find config file path
+            let configPath;
+            if (config) {
+                if (!path.isAbsolute(config)) {
+                    configPath = path.resolve(process.cwd(), config);
+                }
+            }
+            else {
+                configPath = path.resolve(utils.getLavasProjectRoot(), DEFAULT_LAVAS_CONFIG);
+            }
+
+            if (!(await fs.pathExists(configPath))) {
+                log.warn(`${locals.START_NO_FILE} ${configPath}`);
+                return;
+            }
+
+            // read appDir from config file
+            let configJson;
+            try {
+                configJson = require(configPath);
+            }
+            catch (e) {
+                log.warn(`${locals.INPUT_INVALID} ${configPath}`);
+            }
+
+            let appDir = configJson.appDir || utils.getLavasProjectRoot();
+            let serverScriptPath = path.resolve(appDir, DEFAULT_DEV_SERVER_SCRIPT);
 
             let isServerScriptExist = await fs.pathExists(serverScriptPath);
             if (!isServerScriptExist) {
@@ -44,24 +67,11 @@ module.exports = function (program) {
                 process.env.PORT = Number(port);
             }
 
-            let serverScriptFun = require(serverScriptPath);
-            let options = {};
+            let options = [];
             if (config) {
-                if (!path.isAbsolute(config)) {
-                    config = path.resolve(utils.getLavasProjectRoot(), config);
-                }
-
-                if (!(await fs.pathExists(config))) {
-                    log.warn(`${locals.START_NO_FILE} ${config}`);
-                    return;
-                }
-
-                options.config = config;
+                options.push(configPath);
             }
-
-            if (typeof serverScriptFun === 'function') {
-                serverScriptFun(options);
-            }
+            fork(serverScriptPath, options)
         });
 
     // lavas start
@@ -69,15 +79,8 @@ module.exports = function (program) {
         .command('start')
         .description(locals.START_PROD)
         .option('-p, --port <port>', locals.START_PORT)
-        .option('-c, --config [value]', locals.START_CONFIG)
-        .option('-s, --src', locals.START_SCRIPT)
-        .action(async ({port, config, serverScript}) => {
-            let serverScriptPath = path.resolve(
-                utils.getLavasProjectRoot(),
-                serverScript || DEFAULT_PROD_SERVER_SCRIPT
-            );
-
-            log.info(locals.START_PROD_SERVER + '...');
+        .action(async ({port}) => {
+            let serverScriptPath = path.resolve(utils.getLavasProjectRoot(), DEFAULT_PROD_SERVER_SCRIPT);
 
             let isServerScriptExist = await fs.pathExists(serverScriptPath);
             if (!isServerScriptExist) {
@@ -91,23 +94,6 @@ module.exports = function (program) {
                 process.env.PORT = Number(port);
             }
 
-            let serverScriptFun = require(serverScriptPath);
-            let options = {};
-            if (config) {
-                if (!path.isAbsolute(config)) {
-                    config = path.resolve(utils.getLavasProjectRoot(), config);
-                }
-
-                if (!(await fs.pathExists(config))) {
-                    log.warn(`${locals.START_NO_FILE} ${config}`);
-                    return;
-                }
-
-                options.config = config;
-            }
-
-            if (typeof serverScriptFun === 'function') {
-                serverScriptFun(options);
-            }
+            fork(serverScriptPath)
         });
 };
