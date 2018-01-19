3efccebeae1848724b21c10a4d57edc917262afb
Wangyisheng <wangyisheng@baidu.com>
2018-01-19 17:47:46 +0800

    workbox testcase finished

--- packages/lavas-core-vue/test/fixtures/simple/core/service-worker.js
+++ packages/lavas-core-vue/test/fixtures/simple/core/service-worker.js
@@ -4,13 +4,6 @@
  * @author *__ author __*{% if: *__ email __* %}(*__ email __*){% /if %}
  */
 
-/**
- * Import workbox-sw with `importScripts` function.
- * 1. Need adding publicPath manually.
- * 2. If the version of workbox updated, modification is also required.
- */
-importScripts('/static/js/workbox-sw.prod.v2.1.2.js');
-
 const workboxSW = new WorkboxSW({
     cacheId: 'lavas-cache',
     ignoreUrlParametersMatching: [/^utm_/],
@@ -19,7 +12,4 @@ const workboxSW = new WorkboxSW({
 });
 
 // Define precache injection point.
-workboxSW.precache([]);
-
-// Define response for HTML request.
-workboxSW.router.registerNavigationRoute('/appshell/main');
+workboxSW.precache([ ]);
--- packages/lavas-core-vue/test/unit/utils/workbox.test.js
+++ packages/lavas-core-vue/test/unit/utils/workbox.test.js
@@ -5,18 +5,20 @@
 
 import test from 'ava';
 import {join} from 'path';
-import {readFile} from 'fs-extra';
+import {readFile, writeFile} from 'fs-extra';
 import merge from 'webpack-merge';
 import {getWorkboxFiles, useWorkbox} from '../../../dist/utils/workbox';
 import {syncConfig} from '../../utils';
 import LavasCore from '../../../dist';
 
+let core;
+
 test.beforeEach('init', async t => {
-    let core = new LavasCore(join(__dirname, '../../fixtures/simple'));
+    core = new LavasCore(join(__dirname, '../../fixtures/simple'));
     await core.init('production', true);
 });
 
-test('it should get workbox files correctly', t => {
+test.serial('it should get workbox files', t => {
     let devFiles = getWorkboxFiles(false);
     t.deepEqual(devFiles, ['workbox-sw.dev.v2.1.2.js', 'workbox-sw.dev.v2.1.2.js.map']);
 
@@ -24,10 +26,41 @@ test('it should get workbox files correctly', t => {
     t.deepEqual(prodFiles, ['workbox-sw.prod.v2.1.2.js', 'workbox-sw.prod.v2.1.2.js.map']);
 });
 
-test('it should generate service-worker.js correctly in SSR mode', async t => {
+test.serial('it should generate service-worker.js in SSR mode', async t => {
+    await core.build();
+
+    let swContent = await readFile(join(__dirname, '../../fixtures/simple/dist/service-worker.js'), 'utf8');
+
+    t.true(swContent.indexOf('importScripts(\'/static/js/workbox-sw.prod.v2.1.2.js\');') !== -1);
+    t.true(swContent.indexOf('workboxSW.router.registerNavigationRoute(\'/appshell\');') !== -1);
+});
+
+test.serial('it should generate service-worker.js in SSR mode with appshellUrls', async t => {
+    delete core.config.serviceWorker.appshellUrl;
     let config = merge(core.config, {
         build: {
             ssr: true
+        },
+        serviceWorker: {
+            appshellUrls: ['/use-appshell-urls']
+        }
+    });
+    syncConfig(core, config);
+    await core.build();
+
+    let swContent = await readFile(join(__dirname, '../../fixtures/simple/dist/service-worker.js'), 'utf8');
+
+    t.true(swContent.indexOf('workboxSW.router.registerNavigationRoute(\'/use-appshell-urls\');') !== -1);
+});
+
+test.serial('it should generate service-worker.js in SSR mode with baseUrl', async t => {
+    delete core.config.serviceWorker.appshellUrls;
+    let config = merge(core.config, {
+        build: {
+            ssr: true
+        },
+        router: {
+            base: '/some-base/'
         },
         serviceWorker: {
             appshellUrl: '/appshell'
@@ -36,8 +69,94 @@ test('it should generate service-worker.js correctly in SSR mode', async t => {
     syncConfig(core, config);
     await core.build();
 
-    let swContent = await readFile(join(__dirname, '../../fixtures/simple/dist/service-worker.js'));
+    let swContent = await readFile(join(__dirname, '../../fixtures/simple/dist/service-worker.js'), 'utf8');
 
-    t.true(swContent.indexOf('importScripts(\'/static/js/workbox-sw.prod.v2.1.2.js\');') !== -1);
-    t.true(swContent.indexOf('workboxSW.router.registerNavigationRoute(\'/appshell\');') !== -1);
+    t.true(swContent.indexOf('workboxSW.router.registerNavigationRoute(\'/some-base/appshell\');') !== -1);
+});
+
+test.serial('it should generate service-worker.js in SSR mode with invalid config', async t => {
+    let config = merge(core.config, {
+        build: {
+            ssr: true
+        },
+        router: {
+            base: '/base-without-slash'
+        },
+        serviceWorker: {
+            appshellUrl: 'appshell-without-slash'
+        }
+    });
+    syncConfig(core, config);
+    await core.build();
+
+    let swContent = await readFile(join(__dirname, '../../fixtures/simple/dist/service-worker.js'), 'utf8');
+
+    t.true(swContent.indexOf(
+        'workboxSW.router.registerNavigationRoute(\'/base-without-slash/appshell-without-slash\');'
+    ) !== -1);
+});
+
+test.serial('it should generate service-worker.js in SSR mode without appshellUrl', async t => {
+    delete core.config.serviceWorker.appshellUrl;
+    await core.build();
+
+    let swContent = await readFile(join(__dirname, '../../fixtures/simple/dist/service-worker.js'), 'utf8');
+
+    t.true(swContent.indexOf('workboxSW.router.registerNavigationRoute') === -1);
+});
+
+test.serial('it should generate service-worker.js in SPA mode', async t => {
+    let config = merge(core.config, {
+        build: {
+            ssr: false
+        }
+    });
+    syncConfig(core, config);
+    await core.build();
+
+    let swContent = await readFile(join(__dirname, '../../fixtures/simple/dist/service-worker.js'), 'utf8');
+
+    t.true(swContent.indexOf('workboxSW.router.registerNavigationRoute(\'/index.html\');') !== -1);
+});
+
+test.serial('it should generate service-worker.js in SPA mode with baseUrl', async t => {
+    let config = merge(core.config, {
+        build: {
+            ssr: false
+        },
+        router: {
+            base: '/some-base/'
+        }
+    });
+    syncConfig(core, config);
+    await core.build();
+
+    let swContent = await readFile(join(__dirname, '../../fixtures/simple/dist/service-worker.js'), 'utf8');
+
+    t.true(swContent.indexOf('workboxSW.router.registerNavigationRoute(\'/some-base/index.html\');') !== -1);
+});
+
+test.serial('it should generate service-worker.js in SPA mode with invalid config', async t => {
+    let config = merge(core.config, {
+        build: {
+            ssr: false
+        },
+        router: {
+            base: '/base-without-slash'
+        }
+    });
+    syncConfig(core, config);
+
+    let swTemplatePath = join(__dirname, '../../fixtures/simple/core/service-worker.js');
+    let swTemplateContent = await readFile(swTemplatePath, 'utf8');
+    let swTemplateChangedContent = swTemplateContent.replace(/workboxSW\.precache\(\[\]\);/, 'workboxSW.precache([ ]);');
+    await writeFile(swTemplatePath, swTemplateChangedContent, 'utf8');
+
+    await core.build();
+
+    let swContent = await readFile(join(__dirname, '../../fixtures/simple/dist/service-worker.js'), 'utf8');
+
+    t.true(swContent.indexOf('workboxSW.router.registerNavigationRoute(\'/base-without-slash/index.html\');') !== -1);
+
+    await writeFile(swTemplatePath, swTemplateContent, 'utf8');
 });
