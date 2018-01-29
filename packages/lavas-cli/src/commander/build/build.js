1ea337bb00b61218618690bc0d1c8a2f3c233ffd
Yisheng Wang <wangyisheng@baidu.com>
2018-01-29 17:44:58 +0800

    fix: lavas build use local lavas-core-vue (#69)

--- packages/lavas-cli/src/commander/build/build.js
+++ packages/lavas-cli/src/commander/build/build.js
@@ -6,10 +6,28 @@
 'use strict';
 const path = require('path');
 const fs = require('fs-extra');
+const fork = require('child_process').fork;
 
 const utils = require('../../lib/utils');
 const log = require('../../lib/utils/log');
 const locals = require('../../locals')();
+
+async function getConfigPath(config) {
+    if (!config) {
+        return;
+    }
+
+    if (!path.isAbsolute(config)) {
+        config = path.resolve(process.cwd(), config);
+    }
+
+    if (!(await fs.pathExists(config))) {
+        log.warn(`${locals.START_NO_FILE} ${config}`);
+        return;
+    }
+
+    return config;
+}
 
 /**
  * 处理 build 构建逻辑
@@ -20,6 +38,19 @@ module.exports = async function (config) {
     log.info(locals.START_BUILD + '...');
 
     let rootDir = utils.getLavasProjectRoot();
+    let buildScriptPath = path.resolve(rootDir, '.lavas/build.js');
+
+    // 存在.lavas/build.js 直接调用，避免引用全局 lavas-core-vue
+    if (await fs.pathExists(buildScriptPath)) {
+        let options = [];
+        let configPath = await getConfigPath(config);
+        if (configPath) {
+            options.push(configPath);
+        }
+
+        fork(buildScriptPath, options);
+        return;
+    }
 
     // 通过 package.json 中的配置将 lavas-core 从命令行解耦
     // 要求所有的 core 都提供相同的 API
@@ -28,17 +59,8 @@ module.exports = async function (config) {
     let LavasCore = require(lavasConf.core || 'lavas-core-vue');
     let core = new LavasCore(rootDir);
 
-    if (config) {
-        if (!path.isAbsolute(config)) {
-            config = path.resolve(process.cwd(), config);
-        }
+    let configPath = await getConfigPath(config);
 
-        if (!(await fs.pathExists(config))) {
-            log.warn(`${locals.START_NO_FILE} ${config}`);
-            return;
-        }
-    }
-
-    await core.init(process.env.NODE_ENV || 'production', true, {config});
+    await core.init(process.env.NODE_ENV || 'production', true, {config: configPath});
     await core.build();
 };
---
+++ packages/lavas-core-vue/core/templates/build.js
@@ -0,0 +1,20 @@
+var path = require('path');
+
+var LavasCore = require('lavas-core-vue');
+var core = new LavasCore(path.resolve(__dirname, '../'));
+
+var config;
+
+// fix https://github.com/lavas-project/lavas/issues/50
+if (process.argv.length >= 3 && process.argv[2] !== 'build') {
+    config = process.argv[2];
+}
+
+process.env.NODE_ENV = 'production';
+
+async function startBuild() {
+    await core.init(process.env.NODE_ENV, true, {config});
+    await core.build();
+}
+
+startBuild();
