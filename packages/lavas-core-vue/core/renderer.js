a61cdad14fd90f3c207b1daa878453770f5accf9
Wangyisheng <wangyisheng@baidu.com>
2018-03-07 17:33:17 +0800

    修复一个问题

--- packages/lavas-core-vue/core/renderer.js
+++ packages/lavas-core-vue/core/renderer.js
@@ -42,7 +42,7 @@ export default class Renderer {
             return templateName;
         }
 
-        return basename(getTemplatePath());
+        return basename(this.getTemplatePath());
     }
 
     /**
