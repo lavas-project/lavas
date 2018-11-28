752639e5b09f347a3dabbc1bac3975da890ab0e1
wangyisheng <wangyisheng@baidu.com>
2018-11-28 10:23:48 +0800

    修改SW的更新机制

--- packages/lavas-core-vue/core/templates/sw-register.js
+++ packages/lavas-core-vue/core/templates/sw-register.js
@@ -3,17 +3,26 @@
  * @author panyuqi@baidu.com (panyuqi)
  */
 
+function emitUpdate() {
+    var event = document.createEvent('Event');
+    event.initEvent('sw.update', true, true);
+    window.dispatchEvent(event);
+}
+
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js').then(function(reg) {
-        reg.onupdatefound = function() {
+    navigator.serviceWorker.register('/service-worker.js').then(function (reg) {
+        if (reg.waiting) {
+            emitUpdate();
+            return;
+        }
+
+        reg.onupdatefound = function () {
             var installingWorker = reg.installing;
-            installingWorker.onstatechange = function() {
+            installingWorker.onstatechange = function () {
                 switch (installingWorker.state) {
                     case 'installed':
                         if (navigator.serviceWorker.controller) {
-                            var event = document.createEvent('Event');
-                            event.initEvent('sw.update', true, true);
-                            window.dispatchEvent(event);
+                            emitUpdate();
                         }
                         break;
                 }
